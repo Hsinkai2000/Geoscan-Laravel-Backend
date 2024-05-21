74c31b72b13abfb13c91a0de91cf5825ad115561
Hsin Kai <hsinkai2000@gmail.com>
2024-05-21 14:09:45 +0800

    Concentrator Seeder complete
    - Concentrator seeder done with dummy values
    - updated DatabaseSeeder to add in concentrator seeds

--- database/seeders/ConcentratorSeeder.php
+++ database/seeders/ConcentratorSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Concentrator;
 
 class ConcentratorSeeder extends Seeder
 {
@@ -12,6 +13,16 @@ class ConcentratorSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $faker = \Faker\Factory::create();
+        $now = date("Y-m-d H:i:s");
+        Concentrator::create([
+            "project_id" => 1,
+            'device_id' => 'AA818892181234',
+            'remarks' => $faker->text(20),
+            'concentrator_csq' => 20,
+            'concentrator_hp' => 81889218,
+            'created_at' => $now,
+            'updated_at' => $now
+        ]);
     }
 }
