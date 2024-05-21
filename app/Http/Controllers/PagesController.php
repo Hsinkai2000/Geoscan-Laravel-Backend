5d7873a9e5a3c4d617b9c25ac789bc28f96dcb7f
Hsin Kai <hsinkai2000@gmail.com>
2024-05-21 19:24:54 +0800

    set routes in api.php

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -3,11 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Libearies\GeoscanLib;
 
 class PagesController extends Controller
 {
     public function input()
     {
-
+        return response()->json(["message" => "INPUT CALLED"]);
     }
 }
