6ce3ac4c70f6275fa77ca1a73f540ca84cb0b656
Hsin Kai <hsinkai2000@gmail.com>
2024-05-23 18:24:57 +0800

    Successful implementation of TrimStrings

--- routes/api.php
+++ routes/api.php
@@ -4,4 +4,4 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PagesController;
 
-Route::post("/input", [PagesController::class, 'input'])->middleware('TrimStrings');
+Route::post("/input", [PagesController::class, 'input']);
