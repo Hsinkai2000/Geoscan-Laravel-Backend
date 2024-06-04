f256e3bbe2d1a5d5b105746f781389ec5a965ea8
Hsin Kai <hsinkai2000@gmail.com>
2024-06-04 11:03:37 +0800

    Moving lineariseLeq and ConvertToDb to global function

--- app/Functions/GlobalFunctions.php
+++ app/Functions/GlobalFunctions.php
@@ -7,7 +7,6 @@ function debug_log($message, array $context = [])
     Log::channel('debug')->debug($message, $context);
 }
 
-
 function render_message($message)
 {
     response()->json($message, Response::HTTP_OK)->send();
@@ -15,4 +14,14 @@ function render_message($message)
 function render_error(string $error_message)
 {
     response()->json(['error' => $error_message], Response::HTTP_UNPROCESSABLE_ENTITY)->send();
+}
+
+function linearise_leq($leq)
+{
+    return pow(10, $leq / 10);
+}
+
+function convert_to_db($avg_leq)
+{
+    return round(10 * log10($avg_leq), 1);
 }
