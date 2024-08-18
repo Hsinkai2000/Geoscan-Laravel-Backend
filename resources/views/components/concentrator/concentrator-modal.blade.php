056220a389506ff78b08514c54c225f7a939dc1e
Hsin Kai <hsinkai2000@gmail.com>
2024-08-18 12:09:36 +0800

    concentrator modal removing fields

--- resources/views/components/concentrator/concentrator-modal.blade.php
+++ resources/views/components/concentrator/concentrator-modal.blade.php
@@ -28,52 +28,6 @@
                             </div>
                         </div>
 
-                        <div class="mb-3 row">
-                            <label for="concentrator_csq" class="col-md-3 col-sm-12 text-align-center col-form-label">
-                                CSQ</label>
-                            <div class="col-sm-8 align-content-center">
-                                <input type="number" class="form-control" id="inputCSQ" name="concentrator_csq"
-                                    required>
-                            </div>
-                        </div>
-
-                        <div class="mb-3 row">
-                            <label for="concentrator_hp"
-                                class="col-md-3 col-sm-12 text-align-center col-form-label">Handphone Number</label>
-                            <div class="col-sm-8 align-content-center">
-                                <input type="number" class="form-control" id="inputHp" name="concentrator_hp"
-                                    required>
-                            </div>
-                        </div>
-
-                        <div class="mb-3 row">
-                            <label for="battery_voltage"
-                                class="col-md-3 col-sm-12 text-align-center col-form-label">Battery Voltage</label>
-                            <div class="col-sm-8 align-content-center">
-                                <input type="number" class="form-control" id="inputVoltage" name="battery_voltage"
-                                    required>
-                            </div>
-                        </div>
-
-                        <div class="mb-3 row">
-                            <label for="last_communication_packet_sent"
-                                class="col-md-3 col-sm-12 text-align-center col-form-label">Last
-                                Communication Packet Sent</label>
-                            <div class="col-sm-8 align-content-center">
-                                <input type="date" class="form-control" id="inputLastCommunicationPacketSent"
-                                    name="last_communication_packet_sent" required>
-                            </div>
-                        </div>
-
-                        <div class="mb-3 row">
-                            <label for="last_assigned_ip_address"
-                                class="col-md-3 col-sm-12 text-align-center col-form-label">Last
-                                Assigned Ip Address</label>
-                            <div class="col-sm-8 align-content-center">
-                                <input type="text" class="form-control" id="inputLastAssignedIpAddress"
-                                    name="last_assigned_ip_address" required>
-                            </div>
-                        </div>
 
                         <div class="mb-3 row">
                             <label for="remarks"
@@ -85,6 +39,7 @@
                     </div>
 
                     <div class="modal-footer">
+                        <div id="error_message" class="text-danger me-auto"></div>
                         <button type="button" class="btn btn-primary bg-white text-primary"
                             data-bs-dismiss="modal">Discard</button>
                         <button type='submit' onclick="handle_concentrator_submit(event)"
